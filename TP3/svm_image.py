a502a92bbcf3a5b4ae7056dfe0fbcbcfe585dee6
Eliseo Parodi Almaraz <eparodialmaraz@gmail.com>
2019-10-08 14:16:48 -0300

    Add more methods to SVM

--- TP3/svm_image.py
+++ TP3/svm_image.py
@@ -1,12 +1,18 @@
+import os
+import sys
+sys.path.append(os.getcwd())
+
 from common.algorithms.svm import SVM
 from common.experiment.experiment_comparer import TestType, Comparer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import classification_report, confusion_matrix
 from PIL import Image
+from mpl_toolkits.mplot3d import Axes3D
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 cls_images = [
     {
@@ -27,6 +33,8 @@ classes = []
 
 def image_to_df(path):
     colourImg = Image.open(path)
+    width, height = colourImg.size
+    colourImg = colourImg.resize((int(width / 2), int(height / 2)), Image.ANTIALIAS)
     colourPixels = colourImg.convert("RGB")
     colourArray = np.array(colourPixels.getdata()).reshape(
         colourImg.size + (3,))
@@ -49,7 +57,11 @@ df = pd.concat(classes)
 df = df.drop_duplicates()
 
 print("SVM learning")
-svm = SVC(kernel='linear', cache_size=1024*4)
+svm = SVC(kernel='linear', cache_size=1024*4, C=1.0)
+# svm = SVC(kernel='sigmoid', coef0=0.5, cache_size=1024*4)
+# svm = SVC(kernel='sigmoid', coef0=0.8, cache_size=1024*4)
+# svm = SVC(kernel='sigmoid', coef0=1, cache_size=1024*4)
+# svm = SVC(kernel='rbf', cache_size=1024*4, C=1.0, gamma=0.1)
 X_train = df.drop("class",axis=1)
 y_train = df["class"]
 svm.fit(X_train, y_train)
@@ -71,3 +83,18 @@ Image.fromarray(np.uint8(vaca_df.values.reshape(
     height, width, 3))).save("cow.png")
 Image.fromarray(np.uint8(pasto_df.values.reshape(
     height, width, 3))).save("grass.png")
+
+print("Plot image distribution")
+plot_df = df.copy()
+plot_df['color'] = plot_df['class'].replace({
+    "cielo": "blue",
+    "vaca": "brown",
+    "pasto": "green",
+})
+threedee = plt.figure().gca(projection='3d')
+threedee.scatter(plot_df['r'], plot_df['g'],
+                 plot_df['b'], c=plot_df['color'])
+threedee.set_xlabel('r')
+threedee.set_ylabel('g')
+threedee.set_zlabel('b')
+plt.show()
